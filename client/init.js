e423c35b62ac8c3008076135a1b4281d8e35dfa9
Polina Soshnin <psoshnin@wellesley.edu>
2015-12-01 12:27:42 -0800

    add css and images

--- client/init.js
+++ client/init.js
@@ -1,9 +1,8 @@
 $(function() {
-
-//kick off backbone application here
+  //kick off backbone application here
   var messages = new Messages();
   messages.loadMsgs();
   var formView = new FormView({el: $('#main'), collection: messages})
   var messagesView = new MessagesView({el: $('#chats'), collection: messages});
   setInterval( messages.loadMsgs.bind(messages), 1000 );
-}
+});
